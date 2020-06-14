bcdc04cfcf319b1e394691e2db1a96bd3f02edaf
Phelipe Mazzolli <phelipe.mazzolli@gmail.com>
2020-06-14 18:53:52 -0300

    exercicio 13 de array

---
+++ Array/exercicio_13.js
@@ -0,0 +1,8 @@
+/* [O censor] - Faça um programa que receba uma frase e troque a palavra ‘bosta’ por ‘estrume’. (eu sei, nosso trabalho como programador de vez em quando é uma estrume).
+Utilize o método replace()*/
+
+let palavra = "bosta"
+
+let newPalavra = palavra.replace(/bosta/i, 'Estrume')
+
+console.log(newPalavra)
